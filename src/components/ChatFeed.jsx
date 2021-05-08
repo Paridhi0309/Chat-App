51ee2a29acf51c86bd79429b1b61dbb276528d85
Paridhi <4paridhi@gmail.com>
2021-05-08 18:25:53 +0530

    jk

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,7 +7,26 @@ const ChatFeed=(props)=>{
     const chat=chats && chat[activeChat];
     const renderMessages=()=>{
         const keys=Object.keys(messages);
-        console.log(keys);
+       return keys.map((key,index)=>{
+           const message=messages[key];
+           const lastMessageKey=index===0 ?null:keys[index-1];
+           const isMyMessage=userName===message.sender.username;  
+           return(
+               <div key={'msg_${index}'} style={{width:'100%'}}>
+                   <div className="message-block">
+                       {
+                           isMyMessage
+                           ? <MyMessage/>
+                           :<TheirMessage/> 
+                       }
+                   </div>
+                   <div className="read-receipts" style={{marginRight: isMyMessage? '18px' :'0px',marginLeft:isMyMessage ? '0px':'68px'}}>
+                    read-receipts
+                   </div>
+
+               </div>
+           )
+       })
     }
     renderMessages()
     return(
