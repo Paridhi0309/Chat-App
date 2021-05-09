68e6f980733c357a4a9e8bcb5c11c42950467624
Paridhi <4paridhi@gmail.com>
2021-05-09 19:33:27 +0530

    their-message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -4,7 +4,7 @@ import TheirMessage from './TheirMessage';
 
 const ChatFeed=(props)=>{
     const { chats,activeChat,userName,messages }=props;
-    const chat=chats && chat[activeChat];
+    const chat=chats && chats[activeChat];
     const renderMessages=()=>{
         const keys=Object.keys(messages);
        return keys.map((key,index)=>{
@@ -12,12 +12,12 @@ const ChatFeed=(props)=>{
            const lastMessageKey=index===0 ?null:keys[index-1];
            const isMyMessage=userName===message.sender.username;  
            return(
-               <div key={'msg_${index}'} style={{width:'100%'}}>
+               <div key={`msg_${index}`} style={{ width: '100%' }}>
                    <div className="message-block">
                        {
                            isMyMessage
                            ? <MyMessage message={message}/>
-                           :<TheirMessage message={message} lastMessage={messages[lastMessage]} /> //Passing two props here in the their-message component
+                           :<TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
                        }
                    </div>
                    <div className="read-receipts" style={{marginRight: isMyMessage? '18px' :'0px',marginLeft:isMyMessage ? '0px':'68px'}}>
