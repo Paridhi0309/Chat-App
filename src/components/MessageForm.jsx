d6b70521b4eb7e2a6a9dd0e06807903009d99a2d
Paridhi <4paridhi@gmail.com>
2021-05-10 15:37:58 +0530

    Message-Form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,11 @@
 const MessageForm=()=>{
+    const handleSubmit=()=>{
+
+    }
     return(
-        <div>
-            MessageForm
-        </div>
+        <form className="mesage-form" onSubmit={handleSubmit}>
+            <input className="message-input" placeholder="Send a message ..." value={value} onChange={handleChange}/>
+            </form>
     );
 }
 export default MessageForm;
